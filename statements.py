f0ac3ebefa204daf6cf48808e16b257c792c3abf
Jose Ramon Perez <joseramon.perez@BCN-ENL-DEV-078.odigeo.org>
2020-02-03 09:43:07 +0100

    merge statements

--- statements.py
+++ statements.py
@@ -1,3 +1,23 @@
+players = ['cristiano', 'hazard', 'modric', 'benzema']
+for player in players:
+    print(player)
+    #indentation marks the end of the for
+print('hola')
+
+#numeric list
+for value in range(1,6):
+    print(value)
+
+#extract a sort of players
+sort_players = players[0:2]
+print(sort_players)
+
+for player in players[:3]:
+    print(player)
+
+#--------------------------------------------------------
+
+
 cars = ["ford", "bmw", "mercedes", "audi"]
 
 if cars[0] == "bmw":
