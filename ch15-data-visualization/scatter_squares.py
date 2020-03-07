6b66049fd0b7852133f515b5d14b84bf6115b91c
joseramon.perez.jordan@gmail.com <joseramon.perez@odigeo.org>
2020-03-07 17:22:24 +0100

    add random walk

--- ch15-data-visualization/scatter_squares.py
+++ ch15-data-visualization/scatter_squares.py
@@ -4,7 +4,11 @@ x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+# plot in red
+#ax.scatter(x_values, y_values, c='red', s=10)
+
+# plot with color map who emphasis higher values
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -13,5 +17,7 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
